1e8ddd57f7e8ffce90ad1368643d529498013bbf
MhmdJavad <150296769+MWiyPower@users.noreply.github.com>
2025-08-22 01:25:14 +0330

    Update MainActivity.java

--- android/app/src/main/java/su/xash/cs16client/MainActivity.java
+++ android/app/src/main/java/su/xash/cs16client/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.ImageView;
 import android.util.TypedValue;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,9 +51,16 @@ public class MainActivity extends Activity {
             fadeOut.setDuration(500);
             fadeOut.setFillAfter(true);
             fadeOut.setAnimationListener(new Animation.AnimationListener() {
-                @Override public void onAnimationStart(Animation animation) {}
-                @Override public void onAnimationEnd(Animation animation) { launchXash(); }
-                @Override public void onAnimationRepeat(Animation animation) {}
+                @Override
+                public void onAnimationStart(Animation animation) {}
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    launchXash();
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {}
             });
             imageView.startAnimation(fadeOut);
         }, 5000);
@@ -61,56 +69,15 @@ public class MainActivity extends Activity {
     private void launchXash() {
         String pkg = "su.xash.engine.test";
 
-        try { 
-            getPackageManager().getPackageInfo(pkg, 0); 
+        try {
+            getPackageManager().getPackageInfo(pkg, 0);
         } catch (PackageManager.NameNotFoundException e) {
-            try { 
-                pkg = "su.xash.engine"; 
-                getPackageManager().getPackageInfo(pkg, 0); 
+            try {
+                pkg = "su.xash.engine";
+                getPackageManager().getPackageInfo(pkg, 0);
             } catch (PackageManager.NameNotFoundException ex) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/FWGS/xash3d-fwgs/releases/tag/continuous"))
-                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                finish();
-                return;
-            }
-        }
-
-        startActivity(new Intent().setComponent(new ComponentName(pkg, "su.xash.engine.XashActivity"))
-                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                .putExtra("gamedir", "cstrike")
-                .putExtra("gamelibdir", getApplicationInfo().nativeLibraryDir)
-                .putExtra("argv", "-dev 2 -log -dll @yapb")
-                .putExtra("package", getPackageName()));
-        finish();
-    }
-                                         }        fadeIn.setDuration(500);
-        fadeIn.setFillAfter(true);
-        imageView.startAnimation(fadeIn);
-
-        new Handler().postDelayed(() -> {
-            AlphaAnimation fadeOut = new AlphaAnimation(1f, 0f);
-            fadeOut.setDuration(500);
-            fadeOut.setFillAfter(true);
-            fadeOut.setAnimationListener(new Animation.AnimationListener() {
-                @Override public void onAnimationStart(Animation animation) {}
-                @Override public void onAnimationEnd(Animation animation) { launchXash(); }
-                @Override public void onAnimationRepeat(Animation animation) {}
-            });
-            imageView.startAnimation(fadeOut);
-        }, 5000);
-    }
-
-    private void launchXash() {
-        String pkg = "su.xash.engine.test";
-
-        try { 
-            getPackageManager().getPackageInfo(pkg, 0); 
-        } catch (PackageManager.NameNotFoundException e) {
-            try { 
-                pkg = "su.xash.engine"; 
-                getPackageManager().getPackageInfo(pkg, 0); 
-            } catch (PackageManager.NameNotFoundException ex) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/FWGS/xash3d-fwgs/releases/tag/continuous"))
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(
+                        "https://github.com/FWGS/xash3d-fwgs/releases/tag/continuous"))
                         .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
                 finish();
                 return;
